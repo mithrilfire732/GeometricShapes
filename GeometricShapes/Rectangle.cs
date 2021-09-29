0d72c96b2c195f67dc360662a6772a73cac75c51
Alexander Kidd <alexander.t.kidd@gmail.com>
2021-09-29 12:12:54 -0400

    comments

--- GeometricShapes/Rectangle.cs
+++ GeometricShapes/Rectangle.cs
@@ -6,18 +6,18 @@ using System.Threading.Tasks;
 
 namespace GeometricShapes
 {
-    class Rect : Quadrilateral
+    class Rect : Quadrilateral // inherits quad class
     {
-        public int Area()
+        public int Area() // adds area method since not possible in quad
         {
             return LenSide1 * LenSide2;
         }
-        public new void Print()
+        public new void Print() // changes print to print both perimeter and area, new keyword is critical
         {
             Console.WriteLine($"Perimeter: {Perimeter()}, Area: {Area()}");
         }
 
-        public Rect(int SideA, int SideB) : base(SideA, SideB, SideA, SideB) { }
+        public Rect(int SideA, int SideB) : base(SideA, SideB, SideA, SideB) { } // calls quad constructor and assigns the two inputs from Rect to the four in quad
 
     }
 }
